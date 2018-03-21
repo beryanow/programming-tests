0981826a3a5a5fa4b69e7e4a2acdb5b3fda3e362
Максим Берьянов <33306395+beryanow@users.noreply.github.com>
2018-03-21 16:42:59 +0700

    New

--- Test_2 (Polynomials Multiplication)/main.c
+++ Test_2 (Polynomials Multiplication)/main.c
@@ -3,40 +3,51 @@
 #include <stdlib.h>
 
 int num_amount(char *arg, int *i) {
+
     int len1 = strlen(arg);
     *i = len1 - 1;
+
     int max_degree = 0;
     for (int k = 0; k < len1; k++) {
         if (arg[k] == ' ') max_degree++;
     }
+
     return max_degree;
+
 }
 
 double *implementation(int *i, char *arg, int max_degree) {
+
     char *arr = (char *) calloc(sizeof(char), *i + 1);
     double *poly = (double *) calloc(sizeof(double), max_degree + 1);
     int temp_index, degree_index = 0;
+
     while (*i >= 0) {
         for (int k = *i; k >= 0; k--) {
             if (arg[k] == ' ') {
                 temp_index = 0;
+
                 for (int h = k + 1; h <= *i; h++) {
                     arr[temp_index] = arg[h];
                     temp_index++;
                 }
+
                 poly[degree_index] = atof(arr);
                 degree_index++;
+
                 *i = k - 1;
                 break;
             }
             if (k == 0) {
                 temp_index = 0;
+
                 for (int h = k; h <= *i; h++) {
                     arr[temp_index] = arg[h];
                     temp_index++;
                 }
                 poly[degree_index] = atof(arr);
                 degree_index++;
+
                 *i = k - 1;
             }
         }
@@ -45,15 +56,16 @@ double *implementation(int *i, char *arg, int max_degree) {
 }
 
 void factors_print(int deg_1, int deg_2, double *poly_1, double *poly_2) {
-    int m = deg_1 + deg_2;
-    double *mul_table = (double *) calloc(sizeof(double), m + 1);
+    int deg_sum = deg_1 + deg_2;
+    double *mul_table = (double *) calloc(sizeof(double), deg_sum + 1);
+
     for (int i = 0; i <= deg_1; i++) {
         for (int k = 0; k <= deg_2; k++) {
             mul_table[i + k] += poly_1[i] * poly_2[k];
         }
     }
 
-    for (int i = m; i >= 0; i--) {
+    for (int i = deg_sum; i >= 0; i--) {
         printf("%f ", mul_table[i]);
     }
 }
